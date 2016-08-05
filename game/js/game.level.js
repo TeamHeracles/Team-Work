365c719e9a80371669d2ab33edaa309569aa2c29
Daniel Nikolov <horwits1@hotmail.com>
2016-08-05 18:56:45 +0300

    Added new structure Added new main page Changed main page background repeat

---
+++ game/js/game.level.js
@@ -0,0 +1,15 @@
+Game.Level = {
+
+  // Player chooses a game level
+  ,choose : function () {}
+
+
+  // Change game level on certain conditions
+  ,change : function () {}
+
+
+  // End game level on certain conditions
+  ,end : function () {}
+
+
+}
