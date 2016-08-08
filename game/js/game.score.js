02108d069436bdea455d46f71f53d14e6261e718
Desislava Peeva <desi.peeva@gmail.com>
2016-08-08 21:51:09 +0300

    Fix typos

--- game/js/game.score.js
+++ game/js/game.score.js
@@ -4,31 +4,32 @@ Game.Score = {
   add : function (showScore) {
     var forest = document.getElementById("the-canvas"),
         ctx = forest.getContext("2d"),
-	    score = 0;
+        score = 0;
 
-   ctx.font = "22px Verdana";
-   ctx.fillStyle = "#F7931E";
-   ctx.textAlign = "left";
-   ctx.fillText("SCORE: " + parseInt(score), 489, 30);
+    ctx.font = "22px Verdana";
+    ctx.fillStyle = "#F7931E";
+    ctx.textAlign = "left";
+    ctx.fillText("SCORE: " + parseInt(score), 489, 30);
 
   }
 
   // Remove scores method
   ,remove : function (clearScore) {
-	  		if( keycode === restartCode && restartTimer > 0) {
-			restartTimer = 0;
-			gameOn = true;
-			gamePaused = false;
-			score = 0;
-			initApp();
-			run();
+    if (keycode === restartCode && restartTimer > 0) {
+      restartTimer = 0;
+      gameOn = true;
+      gamePaused = false;
+      score = 0;
+      initApp();
+      run();
+    }
   }
 
   // Double the scores on certain conditions
   ,double : function (doubleScore) {
-	  if(certainConditions){
-		  score=* 2;
-	  }
+    if(certainConditions){
+      score= score* 2;
+    }
   }
 
   // Print result
