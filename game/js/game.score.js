b931872bffac63ae67fe6d41742542227c109179
KaloyanchoSt <kaloyanchost@gmail.com>
2016-08-08 19:25:31 +0300

    Added score

--- game/js/game.score.js
+++ game/js/game.score.js
@@ -1,13 +1,35 @@
 Game.Score = {
 
   // Add scores method
-  add : function () {}
+  add : function (showScore) {
+    var forest = document.getElementById("the-canvas"),
+        ctx = forest.getContext("2d"),
+	    score = 0;
+
+   ctx.font = "22px Verdana";
+   ctx.fillStyle = "#F7931E";
+   ctx.textAlign = "left";
+   ctx.fillText("SCORE: " + parseInt(score), 489, 30);
+
+  }
 
   // Remove scores method
-  ,remove : function () {}
+  ,remove : function (clearScore) {
+	  		if( keycode === restartCode && restartTimer > 0) {
+			restartTimer = 0;
+			gameOn = true;
+			gamePaused = false;
+			score = 0;
+			initApp();
+			run();
+  }
 
   // Double the scores on certain conditions
-  ,double : function () {}
+  ,double : function (doubleScore) {
+	  if(certainConditions){
+		  score=* 2;
+	  }
+  }
 
   // Print result
   ,print : function () {}
