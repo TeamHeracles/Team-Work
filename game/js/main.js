3ae1d78cc1c5de7af82d9605c221317271369a50
Desislava Peeva <desi.peeva@gmail.com>
2016-08-09 00:03:28 +0300

    Draw Timer on Game screen, draw grass on Game map

--- game/js/main.js
+++ game/js/main.js
@@ -5,7 +5,7 @@ if(e&&1===a.nodeType)while(c=e[d++])a.removeAttribute(c)}}),hb={set:function(a,b
 
 var Game = {
   game_wrap : $('#game-wrap'),
-  forest : '',
+  forest : '', // Forest map, according to game level
   mouse : {
     id : ''
   },
@@ -21,8 +21,114 @@ var Game = {
 }
 
 Game.Canvas = {
+  forest : '',
+  cheese : '',
+  timer  : '',
+  mouse  : '',
+  step   : 40,
 
-  draw : function ( coords, obj ) {}
+
+  init : function () {
+    // Page content should be loaded before canvas elements can be selected in DOM
+    Game.Canvas.forest = document.getElementById('forest');
+    Game.Canvas.cheese = document.getElementById('cheese');
+    Game.Canvas.timer = document.getElementById('timer');
+    Game.Canvas.mouse = document.getElementById('mouse');
+
+    var game_width = Game.Canvas.forest.width,
+        game_height = Game.Canvas.forest.height;
+
+    var ctx = Game.Canvas.forest.getContext('2d');
+    Game.Canvas.drawMap( Game.level, ctx, game_width, game_height, Game.Canvas.step );
+
+    Game.Timer.init( Game.Canvas.timer );
+  }
+
+
+  ,drawMap : function ( level, ctx, width, height, step ) {
+    var map = Game.Map[level];
+    // console.log( map );
+    for (var row = 0; row < map.length; row++) {
+      for (var col = 0; col < map[row].length; col++) {
+        var cell = map[row][col];
+        if (cell == 'g') {
+          Game.Canvas.drawGrass( ctx, col, row, step );
+        }
+        else if (cell == 'bl') {
+          // Draw Blue tree
+        }
+        else if (cell == 'gr') {
+          // Draw Green tree
+        }
+        else if (cell == 'na') {
+          // Draw Naked tree
+        }
+        else if (cell == 'or') {
+          // Draw Orange tree
+        }
+        else if (cell == 'p') {
+          // Draw Target Point
+        }
+      }
+    }
+  }
+
+  ,drawGrass : function ( ctx, x, y, size ) {
+    // grass code
+    ctx.fillStyle = '#bab5a8';
+    ctx.save();
+    ctx.translate(x*size,y*size);
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(size,0);
+    ctx.lineTo(size,size);
+    ctx.lineTo(0,size);
+    ctx.closePath();
+
+    ctx.clip();
+    ctx.translate(0,0);
+    ctx.translate(0,0);
+    ctx.scale(1,1);
+    ctx.translate(0,0);
+    ctx.strokeStyle = 'rgba(0,0,0,0)';
+    ctx.lineCap = 'butt';
+    ctx.lineJoin = 'miter';
+    ctx.miterLimit = 4;
+    ctx.save();
+    ctx.restore();
+    ctx.save();
+    ctx.save();
+
+    ctx.beginPath();
+    ctx.moveTo(20.796,34.965);
+    ctx.bezierCurveTo(8.944,35.212,-3.677,25.396,0.998,21.422);
+    ctx.bezierCurveTo(3.814,19.029,7.402,28.874000000000002,10.581,27.966);
+    ctx.bezierCurveTo(12.216,27.499,1.231,6.23,7.075,5.063);
+    ctx.bezierCurveTo(11.524000000000001,4.173,10.502,22.168,12.917,23.291999999999998);
+    ctx.bezierCurveTo(18.712,25.56,12.155,11.140999999999998,14.786999999999999,9.034999999999998);
+    ctx.bezierCurveTo(18.293,6.229999999999999,18.230999999999998,15.411999999999999,18.244999999999997,17.564999999999998);
+    ctx.bezierCurveTo(18.206999999999997,19.493,16.551999999999996,25.866999999999997,19.319,26.430999999999997);
+    ctx.bezierCurveTo(23.101,27.241999999999997,22.108999999999998,22.912999999999997,21.866,20.810999999999996);
+    ctx.bezierCurveTo(21.501,19.215999999999998,18.567,12.696999999999996,22.421,12.771999999999997);
+    ctx.bezierCurveTo(25.311999999999998,12.827999999999996,24.293,25.737999999999996,26.101,24.635999999999996);
+    ctx.bezierCurveTo(28.625,23.097999999999995,26.939999999999998,13.653999999999996,28.401,8.522999999999996);
+    ctx.bezierCurveTo(29.179,5.788999999999996,32.117,6.951999999999996,32.306,8.825999999999997);
+    ctx.bezierCurveTo(32.354,12.172999999999996,28.689999999999998,17.342,30.503999999999998,19.79);
+    ctx.bezierCurveTo(32.934999999999995,23.069,33.373999999999995,12.082999999999998,37.851,15.227);
+    ctx.bezierCurveTo(40.423,17.036,30.305999999999997,24.152,30.985,27.113);
+    ctx.bezierCurveTo(31.718,30.31,34.563,23.894,37.245,23.04);
+    ctx.bezierCurveTo(39.516,22.316,41.516,26.021,38.391999999999996,29.134999999999998);
+    ctx.bezierCurveTo(34.639,32.633,26.804,34.84,20.796,34.965);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+    ctx.restore();
+    ctx.restore();
+  }
+
+
+  ,draw : function ( coords, obj ) {}
 
   ,updateObjectState : function () {}
 
@@ -92,8 +198,8 @@ Game.Map = {
     [0,0,0,'g',0,0,'bl','x','p','p',0,0,0,'x','x',0,0,'g','g',0,0,0,0,'p','p'],
     ['g','p','p','p',0,0,'x','x','g','g','g',0,0,'p','p','p','p','g','g','g','g','p','p','p','p']
   ],
-  expert: {},
-  hard: {}
+  expert: [],
+  hard: []
 
 }
 
@@ -103,31 +209,32 @@ Game.Score = {
   add : function (showScore) {
     var forest = document.getElementById("the-canvas"),
         ctx = forest.getContext("2d"),
-	    score = 0;
+        score = 0;
 
-   ctx.font = "22px Verdana";
-   ctx.fillStyle = "#F7931E";
-   ctx.textAlign = "left";
-   ctx.fillText("SCORE: " + parseInt(score), 489, 30);
+    ctx.font = "22px Verdana";
+    ctx.fillStyle = "#F7931E";
+    ctx.textAlign = "left";
+    ctx.fillText("SCORE: " + parseInt(score), 489, 30);
 
   }
 
   // Remove scores method
   ,remove : function (clearScore) {
-	  		if( keycode === restartCode && restartTimer > 0) {
-			restartTimer = 0;
-			gameOn = true;
-			gamePaused = false;
-			score = 0;
-			initApp();
-			run();
+    if (keycode === restartCode && restartTimer > 0) {
+      restartTimer = 0;
+      gameOn = true;
+      gamePaused = false;
+      score = 0;
+      initApp();
+      run();
+    }
   }
 
   // Double the scores on certain conditions
   ,double : function (doubleScore) {
-	  if(certainConditions){
-		  score=* 2;
-	  }
+    if(certainConditions){
+      score= score* 2;
+    }
   }
 
   // Print result
@@ -161,6 +268,7 @@ Game.Screen = {
         // TODO: Draw target points
         // TODO: Draw cheese on separate canvas
         Game.game_wrap.load('game.html', function(){
+          Game.Canvas.init();
           // TODO: Display game Start button
           // TODO: Implement Timer and game Pause button
         });
@@ -185,8 +293,133 @@ Game.Screen = {
 
 Game.Timer = {
 
-  init : function () {}
+  init : function ( timer ) {
+    var ctx = timer.getContext('2d');
+    var cWidth = timer.width;
+    var cHeight = timer.height;
 
-  ,update : function () {}
+    var countTo = 300;
+
+    var min = Math.floor(countTo / 60);
+    var sec = countTo - (min * 60);
+    var counter = 0;
+    var angle = 270;
+    var inc = 360 / countTo;
+
+    function drawScreen() {
+      //======= reset timer
+      // ctx.fillStyle="#786f5b";
+      var gradient = ctx.createRadialGradient(130, 130, 5, 90, 90, 150);
+      gradient.addColorStop(0, '#4f525e');
+      gradient.addColorStop(1, '#333543');
+      ctx.fillStyle = gradient;
+      ctx.fillRect(0, 0, cWidth, cHeight);
+
+      //========== base arc
+      ctx.beginPath();
+      ctx.strokeStyle = "#252424";
+      ctx.lineWidth = 14;
+      ctx.arc(cWidth / 2, cHeight / 2, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+      ctx.stroke();
+      ctx.closePath();
+
+      //========== dynamic arc
+      ctx.beginPath();
+      ctx.strokeStyle = "#df8209";
+      ctx.lineWidth = 14;
+      ctx.arc(cWidth / 2, cHeight / 2, 100, (Math.PI / 180) * 270, (Math.PI / 180) * angle, false);
+      ctx.stroke();
+      ctx.closePath();
+
+      //======== inner shadow arc
+      grad = ctx.createRadialGradient(cWidth / 2, cHeight / 2, 80, cWidth / 2, cHeight / 2, 115);
+
+      grad.addColorStop(0.0, 'rgba(0,0,0,.4)');
+      grad.addColorStop(0.5, 'rgba(0,0,0,0)');
+      grad.addColorStop(1.0, 'rgba(0,0,0,0.4)');
+
+      ctx.beginPath();
+      ctx.strokeStyle = grad;
+      ctx.lineWidth = 14;
+      ctx.arc(cWidth / 2, cHeight / 2, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+      ctx.stroke();
+      ctx.closePath();
+
+      //======== bevel arc
+      grad = ctx.createLinearGradient(cWidth / 2, 0, cWidth / 2, cHeight);
+      grad.addColorStop(0.0, '#6c6f72');
+      grad.addColorStop(0.5, '#252424');
+
+      ctx.beginPath();
+      ctx.strokeStyle = grad;
+      ctx.lineWidth = 1;
+      ctx.arc(cWidth / 2, cHeight / 2, 93, (Math.PI / 180) * 0, (Math.PI / 180) * 360, true);
+      ctx.stroke();
+      ctx.closePath();
+
+      //====== emboss arc
+      grad = ctx.createLinearGradient(cWidth / 2, 0, cWidth / 2, cHeight);
+      grad.addColorStop(0.0, 'transparent');
+      grad.addColorStop(0.98, '#6c6f72');
+
+      ctx.beginPath();
+      ctx.strokeStyle = grad;
+      ctx.lineWidth = 1;
+      ctx.arc(cWidth / 2, cHeight / 2, 107, (Math.PI / 180) * 0, (Math.PI / 180) * 360, true);
+      ctx.stroke();
+      ctx.closePath();
+
+      //====== Labels
+      var textColor = 'white';
+      var textSize = "12";
+      var fontFace = "helvetica, arial, sans-serif";
+
+      ctx.fillStyle = textColor;
+      ctx.font = textSize + "px " + fontFace;
+      ctx.fillText('MIN', cWidth / 2 - 46, cHeight / 2 - 40);
+      ctx.fillText('SEC', cWidth / 2 + 25, cHeight / 2 - 15);
+
+      //====== Values
+      ctx.fillStyle = '#65ae52';
+
+      if (min > 9) {
+        ctx.font = '84px ' + fontFace;
+        ctx.fillText('9', cWidth / 2 - 55, cHeight / 2 + 35);
+
+        ctx.font = '24px ' + fontFace;
+        ctx.fillText('+', cWidth / 2 - 72, cHeight / 2 - 5);
+      }
+      else {
+        ctx.font = '84px ' + fontFace;
+        ctx.fillText(min, cWidth / 2 - 60, cHeight / 2 + 35);
+      }
+
+      ctx.font = '50px ' + fontFace;
+      if (sec < 10) {
+        ctx.fillText('0' + sec, cWidth / 2 + 10, cHeight / 2 + 35);
+      }
+      else {
+        ctx.fillText(sec, cWidth / 2 + 10, cHeight / 2 + 35);
+      }
+
+
+      if (sec <= 0 && counter < countTo) {
+        angle += inc;
+        counter++;
+        min--;
+        sec = 59;
+      } else if (counter >= countTo) {
+        sec = 0;
+        min = 0;
+      } else {
+        angle += inc;
+        counter++;
+        sec--;
+      }
+    }
+
+    setInterval(drawScreen, 1000);
+  }
+
 
 }
